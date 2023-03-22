d22a97867c9f604a75b8483f1019f0021383dce1
Jaesang1998 <tunaonchess01@gmail.com>
2023-03-22 22:59:05 +0900

    split empty card component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,10 @@
 import styled from "@emotion/styled";
 import { Layout } from "@/styled/layout";
 import { Text, TextSizeType } from "@/styled/typography";
-import { CharacterCard } from "../components/home/CharacterCard";
+import {
+  CharacterCard,
+  EmptyCharacterCard,
+} from "../components/home/CharacterCard";
 import { css } from "@emotion/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.css";
@@ -154,47 +157,4 @@ const Background = styled(BaseBox)`
 const Progress = styled(BaseBox)<{ percent: number }>`
   background: ${ColorType.SECONDARY1};
   width: ${({ percent }) => percent}%;
-`;
-
-export const EmptyCharacterCard = () => {
-  return (
-    <CardCard>
-      <FDiv>
-        <Text color={ColorType.NEUTRAL100} type={TextSizeType.KR_CAPTION_01}>
-          캐릭터를 직접 만들어봐요!
-        </Text>
-        <Text color={ColorType.NEUTRAL200} type={TextSizeType.KR_SUB_HEAD_01}>
-          + 캐릭터를 추가할래요~!
-        </Text>
-      </FDiv>
-    </CardCard>
-  );
-};
-
-const CardCard = styled.div`
-  box-sizing: border-box;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 0px;
-  gap: 20px;
-
-  width: 290px;
-  height: 404px;
-
-  border: 2px dashed #6a7395;
-  border-radius: 16px;
-
-  flex: none;
-  order: 3;
-  flex-grow: 0;
-`;
-
-const FDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `;
