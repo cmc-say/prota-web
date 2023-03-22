b5b15a7d55131965609c7fef98ea58c69537d53d
Jaesang1998 <tunaonchess01@gmail.com>
2023-03-22 23:43:05 +0900

    change timeout config to 1000

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  staticPageGenerationTimeout: 120,
+  staticPageGenerationTimeout: 1000,
   images: {
     domains: ["cmc-file-bucket.s3.ap-northeast-2.amazonaws.com"], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
   },
